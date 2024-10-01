28bad9eac6261b8e33742d2eea763a7faeb080ce
Patrick Telnoni <ptelnoni89@gmail.com>
2024-10-01 10:20:18 +0700

    Praktikum Percabangan

---
+++ Percabangan.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;  // Import the Scanner class
+public class Percabangan{
+    public static void main(String[] args){
+        int umur;        
+        
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Masukan umur anda");
+        umur = scanner.nextInt();  // Read user input
+
+        /**
+         * Kerjakan soal di dalam sini
+         * 
+         */
+
+        scanner.close();
+    }
+}
