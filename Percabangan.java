8b0d8f40681458e60c082d12fedb1b94f182c29c
patricktelnoni <patricktelnoni@users.noreply.github.com>
2024-10-02 12:33:46 +0700

    Update Percabangan.java

--- Percabangan.java
+++ Percabangan.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Percabangan{
     public static void main(String[] args){
-        int umur;        
+        int harga;
+        float diskon;
         
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Masukan umur anda");
-        umur = scanner.nextInt();  // Read user input
+        System.out.println("Masukan harga barang");
+        harga = scanner.nextInt();  // Read user input
 
         /**
          * Kerjakan soal di dalam sini
